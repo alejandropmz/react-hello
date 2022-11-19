088b63d6598512bdb1ea16ce3b9490ad59a271e6
alejandropmz <alejandropacheco2497@gmail.com>
2022-11-19 04:56:00 +0000

    18-11 practice

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; // Primero importante importar 
+// tanto useState como useEffect
 import Counter from "./Counter.jsx";
 
-//create your first component
 const Home = () => {
-  const [seconds, setSeconds] = useState(0);
+  //Una vez importado ya podemos usar dentro de nuestra función el useState
+
+  // Se llama la función useState con un arreglo el cual admite 2 parametros.
+
+  // El primer parametro el cual es el estado del componente y el cual podemos actualizar
+
+  // y el segundo parametro es una función la cual podemos llamar cuando queramos actualizar el estado
+  // del primer parámetro.
+
+  const [seconds, setSeconds] = useState(0); 
+
+  //El useEffect 
   useEffect(() => {
-    var interval = setInterval(() => setSeconds(seconds + 1), 1000);
-    console.log(seconds);
+    let interval = setInterval(() => setSeconds(seconds + 1), 1000);
     return () => clearInterval(interval);
   }, [seconds]);
 
