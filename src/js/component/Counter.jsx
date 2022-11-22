7851b0252c18674cd0f2c8073a3d2adf1cb2cf10
alejandropmz <alejandropacheco2497@gmail.com>
2022-11-22 03:23:35 +0000

    last commit

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -4,7 +4,7 @@ const Counter = (props) => {
   return (
     <div className="container">
       <div className="clock">
-        <div><i class="bi bi-clock-fill"></i></div>
+        <div><i class="bi bi-clock-history"></i></div>
         <div className="firstNumber">{props.minutes} :</div>
         <div className="firstNumber">{props.seconds} :</div>
         <div className="firstNumber">{props.miliSeconds}</div>
